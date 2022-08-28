20f1c7992aa80c61a673672053eb7ea59184211f
Sean Coutinho <seancoutinho@my.uopeople.edu>
2022-08-28 13:10:44 +0200

    Initializing calcutot project

---
+++ scripts/calculator.js
@@ -0,0 +1,22 @@
+//this function will add two numbers
+const add = (num1, num2) =>  {
+    return num1 + num2;
+}
+
+//this function will subract the second param from the first one
+const subtract = (num1, num2) => {
+    return num1-num2;
+}
+
+//this function will divide num1 by num2
+const multiply = (num1, num2) => {
+    return num1*num2;
+}
+
+//this function will devide num1 by num2
+const divide = (num1, num2) => {
+    while (num2 != 0) {
+        return num1/num2;
+    } return 'Not a number!'
+}
+
